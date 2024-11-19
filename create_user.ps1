3602ad04fd8ad7df4dc26a6df8d09a8440ea2f2e
Joan Boulangger <110940433+jboulangger@users.noreply.github.com>
2024-11-19 15:37:15 -0500

    Cambios realizados

--- create_user.ps1
+++
@@ -1,6 +0,0 @@
-$USER="USUARIO"
-$PASSWORD="PASSWORD"
-$DESCRIPTION="DESCRIPTION"
-$GROUP="GROUP"
-New-LocalUser -Name $USER -Password (ConvertTo-SecureString -AsPlainText $PASSWORD -Force) -Description $DESCRIPTION
-Add-LocalGroupMember -Group $GROUP -Member $USER
